b768b834dba79c349587f0453efff9d41886524f
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-12-21 23:52:48 +0300

    moving to Sign up activity

--- app/src/main/java/alaiz/hashim/firebaseauthentication/LoginActivity.kt
+++ app/src/main/java/alaiz/hashim/firebaseauthentication/LoginActivity.kt
@@ -1,5 +1,6 @@
 package alaiz.hashim.firebaseauthentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,11 +9,17 @@ import android.widget.TextView
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
-
+    lateinit var loginBtn:Button
+    lateinit var signUpTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        loginBtn=findViewById(R.id.login_button1)
+        signUpTextView=findViewById(R.id.sing_up_text_view)
+        signUpTextView.setOnClickListener{
+        val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
