24c6fc087ec7c4cec3806609ec1ebb9579dc174c
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-12-21 23:46:16 +0300

    init firebase auth project

---
+++ app/src/main/java/alaiz/hashim/firebaseauthentication/LoginActivity.kt
@@ -0,0 +1,19 @@
+package alaiz.hashim.firebaseauthentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+
+}
